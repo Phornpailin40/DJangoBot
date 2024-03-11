95104d97dabab1db4efe64d63ed7dc68c8e17dfa
Phornpailin Lertudomkitpaisan <sa22055@student.samk.fi>
2024-03-11 22:09:52 +0200

    all file

---
+++ chatbot/backend/responses.py
@@ -0,0 +1,7 @@
+def bot_response(message):
+    if message == 'hello':
+        return 'Hello there humanbeing'
+    elif message == 'goodbye':
+        return 'Short circuit executed'
+    else:
+        return 'Cannot comprehend'
---
+++ chatbot/frontend/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def frontend(request, slug=None):
+    #return HttpResponse('<p>Hello from frontend-side</p>')
+    return render(request, 'frontend/template_chatbot.html')
